a11f0a0b951ea819b95a348178d16b40e2da5393
lucasa98 <lusaurin@gmail.com>
2025-10-31 16:04:12 -0300

    mover oveja solo bajo estimulo

--- scripts/run_sim.py
+++ scripts/run_sim.py
@@ -1,26 +1,30 @@
 import pygame
+from numpy.random import default_rng, Generator
+import numpy as np
 from sim.world import World
 
 # ============================================================
 # ======================== PARAMETROS ========================
 # ============================================================
+rng: Generator = default_rng()
 params = {
     # OVEJAS
-    "r_s": 65.0,  # radio de repulsion del pastor
-    "r_a": 2.0,  # radio de repulsion de otras ovejas
-    "h": 0.5,  # coeficiente de inercia
-    "c": 1.05,  # coeficiente de cohesion
-    "rho_a": 2.0,  # fuerza de repulsion (oveja-oveja)
-    "rho_s": 1.0,  # fuerza de repulsion (pastor-oveja)
-    "e": 0.3,  # ruido angular (componente estocastica)
-    "delta": 1.0,  # distancia por paso
-    "n_neigh": 5,  # numero de vecinos para cohesion
+    "r_s": 65.0,    # radio de repulsion del pastor
+    "r_a": 2.0,     # radio de repulsion de otras ovejas
+    "h": 0.5,       # coeficiente de inercia
+    "c": 1.05,      # coeficiente de cohesion
+    "rho_a": 2.0,   # fuerza de repulsion (oveja-oveja)
+    "rho_s": 1.0,   # fuerza de repulsion (pastor-oveja)
+    "e": 0.3,       # ruido angular (componente estocastica)
+    "delta": 1.0,   # distancia por paso
+    "n_neigh": 5,   # numero de vecinos para cohesion
+    "r_walk": 0.1  # probabilidad de random walk
 }
 # ==========================================================
 
 pygame.init()
 screen: pygame.Surface = pygame.display.set_mode((800, 600))
-world = World(800, 600, params)
+world = World(800, 600, params, rng)
 clock = pygame.time.Clock()
 dt = 0
 
--- sim/world.py
+++ sim/world.py
@@ -1,17 +1,18 @@
-import numpy as np
+from numpy.random import Generator
 import pygame
 from models.sheep import Sheep
 
 
 class World:
-    def __init__(self, width, height, params):
+    def __init__(self, width, height, params, rng: Generator):
         self.width = width
         self.height = height
+        self.rng = rng
         N = 50
-        rand_positions = np.random.uniform(0, 1, size=(N, 2))
+        rand_positions = rng.uniform(0, 1, size=(N, 2))
         rand_positions[:, 0] *= self.width
         rand_positions[:, 1] *= self.height
-        self.entities = [Sheep(rand_positions[i], 0, params) for i in range(50)]
+        self.entities = [Sheep(rand_positions[i], 0, params, rng) for i in range(50)]
 
     def update(self, dt):
         for e in self.entities:
