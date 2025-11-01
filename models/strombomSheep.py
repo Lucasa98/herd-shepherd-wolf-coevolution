e902dc5baa8cdf28832559a1f833ec025cfa34b4
lucasa98 <lusaurin@gmail.com>
2025-10-31 21:31:28 -0300

    mejorar modelo de oveja

--- models/strombomSheep.py
+++ models/strombomSheep.py
@@ -15,18 +15,37 @@ class StrombomSheep:
 
         # ===== Calcular fuerzas =====
         # repulsion de los shepherds
+        sheep.pastoreada = False
         R_s = np.zeros(2)
         for s in shepherds:
             diff = sheep.position - s.position
             dist = np.linalg.norm(diff)
             if dist < p["r_s"]:
+                sheep.pastoreada = True
                 R_s += diff / dist
 
-        sheep.repelido = (R_s != np.zeros(2)).any()
+        # repulsion local de vecinas
+        repelida = False
+        R_a = np.zeros(2)
+        for other in sheeps:
+            if other is sheep:
+                continue
+            diff = sheep.position - other.position
+            dist = np.linalg.norm(diff)
+            if dist < p["r_a"] and dist > 0:
+                repelida = True
+                R_a += diff / dist
 
-        # Solo se mueve por random walk o porque el pastor se acerco
-        if sheep.repelido or self.rng.uniform(0, 1) < p["r_walk"]:
-            # Atraccion al centro de gravedad
+        # Si no hay repulsion de ningun tipo ni random walk, TERMINAMOS
+        if not sheep.pastoreada and (R_a == np.zeros(2)).all() and self.rng.uniform(0, 1) > p["r_walk"]:
+            return
+
+        # ruido
+        noise = np.random.uniform(-1, 1, 2)
+
+        # Atraccion al centro de gravedad (SOLO si hay repulsion de pastor)
+        C_i = np.zeros(2)
+        if sheep.pastoreada:
             diffs = np.array(
                 [
                     other.position - sheep.position
@@ -42,29 +61,18 @@ class StrombomSheep:
                 else np.zeros(2)
             )
 
-            # repulsion local de vecinas
-            R_a = np.zeros(2)
-            for other in sheeps:
-                if other is sheep:
-                    continue
-                diff = sheep.position - other.position
-                dist = np.linalg.norm(diff)
-                if dist < p["r_a"] and dist > 0:
-                    R_a += diff / dist
+        # ===== Combinar =====
+        H_new = (p["h"] * sheep.heading + p["e"] * noise)
 
-            # ===== Combinar =====
-            noise = np.random.uniform(-1, 1, 2)
-            H_new = (
-                p["h"] * sheep.heading
-                + p["c"] * C_i
-                + p["rho_a"] * R_a
-                + p["rho_s"] * R_s
-                + p["e"] * noise
-            )
+        if sheep.pastoreada:
+            H_new += (p["c"] * C_i + p["rho_s"] * R_s)
 
-            # Normalizar
-            H_new /= np.linalg.norm(H_new) + 1e-8
-            sheep.heading = H_new
+        if repelida:
+            H_new += (p["rho_a"] * R_a)
 
-            # ===== Update =====
-            sheep.position += p["delta"] * sheep.heading * dt
+        # Normalizar
+        H_new /= np.linalg.norm(H_new) + 1e-8
+        sheep.heading = H_new
+
+        # ===== Update =====
+        sheep.position += p["delta"] * sheep.heading * dt
--- sim/sheep.py
+++ sim/sheep.py
@@ -8,13 +8,13 @@ class Sheep:
         self.position = np.array(position, dtype=float)
         self.heading = np.array(heading, dtype=float)
         self.model = model
-        self.repelido = False
+        self.pastoreada = False
 
     def update(self, sheeps, shepherd: list[Shepherd], dt=1.0):
         # delegar el comportamiento al modelo
         self.model.update(self, sheeps, shepherd, dt)
 
     def draw(self, surface: pygame.Surface):
-        color = "blue" if self.repelido else "red"
-        pygame.draw.circle(surface, color, self.position, 5)
-        pygame.draw.line(surface, "white", self.position, self.position + self.heading * 10, 1)
+        color = "blue" if self.pastoreada else "red"
+        pygame.draw.circle(surface, color, self.position, 1)
+        #pygame.draw.line(surface, "white", self.position, self.position + self.heading * 10, 1)
