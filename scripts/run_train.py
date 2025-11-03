9a2eaa817cbbf1245b519e98b004b4ca5cae8b7b
lucasa98 <lusaurin@gmail.com>
2025-11-03 15:33:35 -0300

    paralelizacion

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 import yaml
 import numpy as np
 from tqdm import tqdm
@@ -6,8 +7,6 @@ from training.evaluador import Evaluador
 
 with open("config.yaml") as f:
     params = yaml.safe_load(f)
-
-rng: np.random.Generator = np.random.default_rng()
 
 ventana = params["poblacion"] // params["progenitores"]
 params["n_inputs"] = (
@@ -29,61 +28,99 @@ params["n_bits"] = 8 * (
     + (params["hidden_lay_2"] * 2 + 2)  # capa de salida: 2 neuronas de salida, FIJO
 )
 
-evaluador = Evaluador(params, rng)
-fit_history = np.empty((0, 2), dtype=float)
+rng: np.random.Generator = np.random.default_rng()
+
+
+# inicializador de cada hilo
+def worker_loop(in_q: mp.Queue, out_q: mp.Queue, params):
+    local_rng: np.random.Generator = np.random.default_rng()
+    ev = Evaluador(params, local_rng)
+    for i, genome in iter(in_q.get, None):
+        out_q.put((i, ev.evaluar(genome)))
+
+
+def evaluar_poblacion(poblacion, in_q, out_q):
+    fit = [0 for _ in range(len(poblacion))]
+    for i, gen in enumerate(poblacion):
+        in_q.put((i, gen))
+    for _ in range(len(poblacion)):
+        i, f = out_q.get()
+        fit[i] = f
+    return fit
+
+
+# ====================================
 
 # ======================= EVOLUCION =======================
-# 1) inicializar la poblacion al azar
-poblacion = rng.integers(0, 2, (params["poblacion"], params["n_bits"]), dtype=np.uint8)
 
-# 2) calcular fitness
-fit = [
-    evaluador.evaluar(poblacion[i]) for i in range(params["poblacion"])
-]  # TODO: paralelizar
-sorted = np.argsort(fit)  # indices que ordenan de menor a mayor
+if __name__ == "__main__":  # esto lo necesita multiprocessing para no joder
+    N_WORKERS = 6  # OJO!
+    # Arrancar workers
+    in_q, out_q = mp.Queue(), mp.Queue()
+    workers: list[mp.Process] = [
+        mp.Process(target=worker_loop, args=(in_q, out_q, params))
+        for _ in range(N_WORKERS)
+    ]
+    for w in workers:
+        w.start()
 
-c_tol: int = 0  # contador de generaciones sin mejora
-fit_elite = fit[sorted[-1]]
-fit_history = np.vstack([fit_history, [0, fit_elite]])
-for g in tqdm(range(params["generaciones"])):
-    # 1) elegir progenitores: un elite y el resto por ventana
-    progenitores = np.empty((params["progenitores"], params["n_bits"]), dtype=np.uint8)
-    progenitores[0] = poblacion[sorted[-1]]  # elite
+    # 1) inicializar la poblacion al azar
+    poblacion = rng.integers(
+        0, 2, (params["poblacion"], params["n_bits"]), dtype=np.uint8
+    )
+    fit_history = np.empty((0, 2), dtype=float)
 
-    v = ventana
-    for i in range(1, params["progenitores"]):
-        progenitores[i] = poblacion[sorted[-rng.integers(0, v, 1)]]
-        v += ventana
-
-    # 2) cruzar progenitores (cruza simple)
-    poblacion[: params["progenitores"]] = progenitores
-    for i in range(params["progenitores"], params["poblacion"]):
-        p1, p2 = rng.integers(
-            0, params["progenitores"], 2
-        )  # tomar progenitores al azar
-
-        # punto de cruza: elegir inicio y fin del segmento a cruzar
-        c1, c2 = np.sort(rng.integers(0, params["n_bits"], 2))
-
-        # cruzar: segmento del padre 1 y el resto del padre 2
-        poblacion[i, :c1] = progenitores[p2, :c1]  # antes de c1
-        poblacion[i, c1:c2] = progenitores[p1, c1:c2]  # c1 a c2
-        poblacion[i, c2:] = progenitores[p2, c2:]  # de c2 al final
-
-        # mutar
-        if rng.random() < params["mutacion"]:
-            b = rng.integers(0, params["n_bits"])
-            poblacion[i, b] ^= 1  # invierte 0 a 1 y viceversa
-
-    # 3) evaluar fitness
-    fit = [
-        evaluador.evaluar(poblacion[i]) for i in range(params["poblacion"])
-    ]  # TODO: paralelizar
+    # 2) calcular fitness
+    fit = evaluar_poblacion(poblacion, in_q, out_q)
     sorted = np.argsort(fit)  # indices que ordenan de menor a mayor
-    if fit[sorted[-1]] > fit_elite:
-        c_tol = 0
-        fit_elite = fit[sorted[-1]]
-        fit_history = np.vstack([fit_history, [g + 1, fit_elite]])
 
-print(fit_elite)
-print(fit_history)
+    fit_elite = fit[sorted[-1]]
+    fit_history = np.vstack([fit_history, [0, fit_elite]])
+    for g in tqdm(range(params["generaciones"])):
+        # 1) elegir progenitores: un elite y el resto por ventana
+        progenitores = np.empty(
+            (params["progenitores"], params["n_bits"]), dtype=np.uint8
+        )
+        progenitores[0] = poblacion[sorted[-1]]  # elite
+
+        v = ventana
+        for i in range(1, params["progenitores"]):
+            progenitores[i] = poblacion[sorted[-rng.integers(0, v, 1)]]
+            v += ventana
+
+        # 2) cruzar progenitores (cruza simple)
+        poblacion[: params["progenitores"]] = progenitores
+        for i in range(params["progenitores"], params["poblacion"]):
+            p1, p2 = rng.integers(
+                0, params["progenitores"], 2
+            )  # tomar progenitores al azar
+
+            # punto de cruza: elegir inicio y fin del segmento a cruzar
+            c1, c2 = np.sort(rng.integers(0, params["n_bits"], 2))
+
+            # cruzar: segmento del padre 1 y el resto del padre 2
+            poblacion[i, :c1] = progenitores[p2, :c1]  # antes de c1
+            poblacion[i, c1:c2] = progenitores[p1, c1:c2]  # c1 a c2
+            poblacion[i, c2:] = progenitores[p2, c2:]  # de c2 al final
+
+            # mutar
+            if rng.random() < params["mutacion"]:
+                b = rng.integers(0, params["n_bits"])
+                poblacion[i, b] ^= 1  # invierte 0 a 1 y viceversa
+
+        # 3) evaluar fitness
+        fit = evaluar_poblacion(poblacion, in_q, out_q)
+        sorted = np.argsort(fit)  # indices que ordenan de menor a mayor
+        if fit[sorted[-1]] > fit_elite:
+            fit_elite = fit[sorted[-1]]
+            fit_history = np.vstack([fit_history, [g + 1, fit_elite]])
+
+    # Detener workers
+    for _ in range(N_WORKERS):
+        in_q.put(None)
+    for w in workers:
+        w.join()
+
+    print(f"Best fitness: {fit_elite}")
+    print("fit history:")
+    print(fit_history)
